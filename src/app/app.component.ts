75d982d2ea2d247779728f88a4af8b84e152b9db
fahrulalwan <mohammad.alwan@pji-solutions.com>
2019-05-13 23:29:10 +0700

    testing sal.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as sal from 'sal.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fahrulalwan';
+
+  ngOnInit(): void {
+    sal({
+      once: false
+    });
+  }
 }
