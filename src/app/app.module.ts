5c03ebdc6ee3eed54e471913ffa1ce951633b788
mohammad.alwan <mohammad.alwan@pji-solutions.com>
2019-12-14 21:39:14 +0700

    SSR: enable user hit page directly using SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './_components/header/header.component';
 import { FooterComponent } from './_components/footer/footer.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { UniversalInterceptor } from './_services/universal-interceptor/universal.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,9 @@ import { FooterComponent } from './_components/footer/footer.component';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     AppRouting
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: UniversalInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
